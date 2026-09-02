d10a32a914145c6e42831b5362f72236af756492
Royko7 <ura.royko@gmail.com>
2026-09-02 12:25:07 +0300

    add fix classNames

--- src/App.jsx
+++ src/App.jsx
@@ -234,49 +234,17 @@ export const App = () => {
                       {item.category.icon} - {item.category.title}
                     </td>
 
-                    <td data-cy="ProductUser" className="has-text-link">
+                    <td
+                      data-cy="ProductUser"
+                      className={classNames({
+                        'has-text-link': item.user.sex === 'm',
+                        'has-text-danger': item.user.sex === 'f',
+                      })}
+                    >
                       {item.user.name}
                     </td>
                   </tr>
                 ))}
-                {/* <tr data-cy="Product">
-                <td className="has-text-weight-bold" data-cy="ProductId">
-                  1
-                </td>
-
-                <td data-cy="ProductName">Milk</td>
-                <td data-cy="ProductCategory">🍺 - Drinks</td>
-
-                <td data-cy="ProductUser" className="has-text-link">
-                  Max
-                </td>
-              </tr>
-
-              <tr data-cy="Product">
-                <td className="has-text-weight-bold" data-cy="ProductId">
-                  2
-                </td>
-
-                <td data-cy="ProductName">Bread</td>
-                <td data-cy="ProductCategory">🍞 - Grocery</td>
-
-                <td data-cy="ProductUser" className="has-text-danger">
-                  Anna
-                </td>
-              </tr>
-
-              <tr data-cy="Product">
-                <td className="has-text-weight-bold" data-cy="ProductId">
-                  3
-                </td>
-
-                <td data-cy="ProductName">iPhone</td>
-                <td data-cy="ProductCategory">💻 - Electronics</td>
-
-                <td data-cy="ProductUser" className="has-text-link">
-                  Roma
-                </td>
-              </tr> */}
               </tbody>
             </table>
           ) : (
